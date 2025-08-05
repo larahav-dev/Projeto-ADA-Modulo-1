b69594ce3e3fa9a5c942c7bc99220844307458d5
lirasusejdev <lirasusejarts@gmail.com>
2025-08-05 20:30:15 -0300

    feat: Adiciona a classe Ator
    Cria a classe Ator.java na pasta Entity com os atributos nome, data de nascimento e nacionalidade. Inclui construtor, getters, setters, toString, equals e hashCode.

--- Projeto ADA/src/Entity/Ator.java
+++ Projeto ADA/src/Entity/Ator.java
@@ -1,5 +1,64 @@
 package Entity;
 
-// teste commit
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Ator {
+
+    private String nome;
+    private LocalDate dataNascimento;
+    private String nacionalidade;
+
+    public Ator(String nome, LocalDate dataNascimento, String nacionalidade) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Ator: " +
+                "nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", nacionalidade='" + nacionalidade + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ator ator = (Ator) o;
+        return Objects.equals(nome, ator.nome) && Objects.equals(dataNascimento, ator.dataNascimento) && Objects.equals(nacionalidade, ator.nacionalidade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, dataNascimento, nacionalidade);
+    }
 }
